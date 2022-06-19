76f4ea00a533bf507f5083f5b67d043c03b95716
Winston Walter <winstonwalter@Winstons-Air.home>
2022-06-19 15:09:26 -0400

    initial work on the v2

--- app2.py
+++ app2.py
@@ -3,13 +3,22 @@ import os
 import json
 
 class Nodes:
-    def __init__(self, json_data, timestamp: bool):
-        self.json_data = json_data
+    def __init__(self, json_data, timestamp: bool, directory: str):
+        os.chdir("/")
+        print(os.curdir)
+        """enter the fill directory, enter the json data that you want in the file,
+        and enter True or False if you want to add a time stamp of the creation
+        date"""
         
+        self.json_data = json_data
+        self.directory = os.chdir(directory)
         if timestamp:
             self.timestamp = datetime.now()
+        else:
+            self.timestam = None
 
-        print(self.timestamp)
+        #checking what id to make the next file
 
+        print(os.listdir("."))      
 
-Nodes(json_data = {}, timestamp=True)
+Nodes(json_data = {}, directory = "/Users/winstonwalter/Desktop/file-node-chain/app2.py", timestamp=True) 
