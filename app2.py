a9ec2f017087594ee2bd92df7c9efb17c5a147b8
Winston Walter <winstonwalter@Winstons-Air.home>
2022-06-21 19:37:32 -0400

    main functions
    the program is not saving some changes so i was going to commit this later but maybe this will solve problem

--- app2.py
+++ app2.py
@@ -1,24 +1,56 @@
 from datetime import datetime
 import os
 import json
+import platform
+from time import time
+
 
 class Nodes:
-    def __init__(self, json_data, timestamp: bool, directory: str):
-        os.chdir("/")
-        print(os.curdir)
+    def __init__(self, directory: str):
         """enter the fill directory, enter the json data that you want in the file,
         and enter True or False if you want to add a time stamp of the creation
         date"""
+
         
+
+        self.dir = directory
+
+        self.WHICH_OS = platform.platform()
+
+        if "macos" in self.WHICH_OS.lower():
+            self.mac_process()
+
+            #check what id to make the next file
+
+    #file system different between windows and mac so this is jsut for mac os    
+    def mac_process(self):
+
+            #end goal is to get the chdir to be the disk aka /
+            INITIAL_CWD = os.getcwd()
+            for file in range(len(INITIAL_CWD)):
+                if INITIAL_CWD[file] == "/":
+                    os.chdir("../")
+
+    
+    def makeNodeObject(self, json_data, timestamp: bool):
         self.json_data = json_data
-        self.directory = os.chdir(directory)
+
         if timestamp:
             self.timestamp = datetime.now()
         else:
-            self.timestam = None
+            self.timestamp = None
+        
+        print(os.listdir(self.directory))
 
-        #checking what id to make the next file
 
-        print(os.listdir("."))      
+    def deleteNodeObject(self):
+        pass
 
-Nodes(json_data = {}, directory = "/Users/winstonwalter/Desktop/file-node-chain/app2.py", timestamp=True) 
+    def findNodeObject(self):
+        pass
+      
+            
+
+new = Nodes(directory = "/Users/winstonwalter/Desktop/file-node-chain/app2.py") 
+
+new.makeNodeObject(json_data={}, timestamp=True)
